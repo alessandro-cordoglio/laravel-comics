aede53311e3f1f16dab3111c7335a35cbb92460e
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-01-16 21:20:24 +0100

    aggiunto stile header + hero

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,31 @@
 <header>
-    
+    <div class="header_top py-2">
+        <div class="container d-flex justify-content-end">
+            <a href="#">Link</a>
+            <a href="#">Dropdown</a>
+        </div>
+    </div>
+    <div class="header_bottom">
+        <div class="header_container container">
+            <div class="logo">
+                <img src="{{Vite::asset('resources/imgs/dc-logo.png')}}" alt="">
+            </div>
+            <nav class="nav_layout">
+                <ul>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <section class="jumbotron">
+    </section>
 </header>
