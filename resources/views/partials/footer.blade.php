5f7bceb4018ad65da611b1d63d0d6152d4c8e92c
alessandro-cordoglio <alessandro.cord20@gmail.com>
2023-01-16 21:30:51 +0100

    aggiunto stile footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,123 @@
 <footer>
+    <section class="item_section">
+        <div class="container_">
+          <div class="items">
+            <div class="item" >
+              <div class="item_info">LINK</div>
+              <div class="item_info">LINK</div>
+              <div class="item_info">LINK</div>
+              <div class="item_info">LINK</div>
+              <div class="item_info">LINK</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="small_info--section">
+        <div class="container_ small_info">
+          <div class="left_footer">
+            <div class="info_block first_info">
+              <div class="info_list">
+                <div class="info_title">
+                  <h3>DC COMICS</h3>
+                </div>
+                <div>
+                  <ul class="list_elements">
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Chara</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Cacters</a></li>
+                    <li><a href="#">Charars</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Charaters</a></li> 
+                  </ul>
+                </div>
+              </div>
+              <div class="first_info--second_block">
+                <div class="info_list">
+                <div class="info_title ">
+                  <h3>SHOP</h3>
+                </div>
+                <div>
+                  <ul class="list_elements">
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Chara</a></li>
+                    <li><a href="#">Characters</a></li>
+                  </ul>
+                </div>
+              </div>
+              </div>
+            </div>
+            <div class="info_block second_info">
+              <div class="info_list">
+                <div class="info_title">
+                  <h3>DC</h3>
+                </div>
+                <div>
+                  <ul class="list_elements">
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Chara</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Cacters</a></li>
+                    <li><a href="#">Charars</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Charaters</a></li> 
+                    <li><a href="#">Cacters</a></li>
+                    <li><a href="#">Charars</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Charaters</a></li> 
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="info_block third_info">
+              <div class="info_list">
+                <div class="info_title">
+                  <h3>SITES</h3>
+                </div>
+                <div>
+                  <ul class="list_elements">
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Chara</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Cacters</a></li>
+                    <li><a href="#">Charars</a></li>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Charaters</a></li> 
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
     
+          <div class="right_footer">
+            <div class="right_footer_bg">
+              <img src="{{Vite::asset('resources/imgs/dc-logo-bg')}}" alt="">
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="sign_up--section">
+        <div class="container_ sign_up">
+          <div class="sign_up--left">
+            <div class="btn">
+              <button class="main_button">
+                  SIGN-UP NOW!
+              </button>
+            </div>
+          </div>
+          <div class="sign_up--right">
+            <div class="sign_up--title">
+              <span>FOLLOW US</span>
+            </div>
+            <div class="sign_up_social flex">
+              <div><img src="../assets/footer-facebook.png" alt=""></div>
+              <div><img src="../assets/footer-twitter.png" alt=""></div>
+              <div><img src="../assets/footer-youtube.png" alt=""></div>
+              <div><img src="../assets/footer-pinterest.png" alt=""></div>
+              <div><img src="../assets/footer-periscope.png" alt=""></div>
+             
+            </div>
+          </div>
+        </div>
+      </section>
 </footer>
